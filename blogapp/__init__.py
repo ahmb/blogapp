ed79562fb7e9ae949eed2dc004a0a0bbe55e825f
raufguy <raufguy@gmail.com>
2014-10-18 22:55:00 -0400

    ver 0.6 Password Hashing

--- blogapp/__init__.py
+++ blogapp/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from .blog import blog as blog_blueprint
 from config import config
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
+db = SQLAlchemy()
 bootstrap = Bootstrap()
 
 
@@ -13,6 +15,9 @@ def create_app(config_name):
 
     # add all the various wrappers here
     bootstrap.init_app(app)
+    db.init_app(app)
+
+
 
     app.register_blueprint(blog_blueprint)
 
