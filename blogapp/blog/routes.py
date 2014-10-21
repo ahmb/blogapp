5981b160d2f98474386278ccea8ba2e9abb5845f
raufguy <raufguy@gmail.com>
2014-10-21 18:37:55 -0400

    ver 0.7.13 Edit profile

--- blogapp/blog/routes.py
+++ blogapp/blog/routes.py
@@ -1,6 +1,9 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from flask.ext.login import login_required, current_user
+from .. import db
 from . import blog
 from ..models import User
+from .forms import ProfileForm
 
 @blog.route('/')
 def index():
@@ -11,3 +14,21 @@ def index():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('blog/user.html', user=user)
+
+@blog.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    form = ProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.bio = form.bio.data
+        db.session.add(current_user._get_current_object())
+        db.session.commit()
+        flash('Your profile has been updated.')
+        return redirect(url_for('talks.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.bio.data = current_user.bio
+    return render_template('talks/profile.html', form=form)
+
